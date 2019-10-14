d59d053fcade8c464a8c9806f75124b9a8636dd9
Evgeny <evgeny.gr81@gmail.com>
2019-10-14 14:38:53 +0300

    Logger update
    Will create logs dir if not exists

--- logger.py
+++ logger.py
@@ -1,11 +1,18 @@
 import logging
+import os
+
+from config import config
 
 log_format = "%(asctime)s: %(filename)s %(funcName)s :%(lineno)s => %(message)s"
 
 logging.basicConfig(level=logging.DEBUG, format=log_format)
 log = logging.getLogger("http_server")
-file_handler = logging.FileHandler("logs/server.log")
-file_handler.setFormatter(format)
+
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
+file_handler = logging.FileHandler(f"logs/{config.get('DEFAULT', 'LogFileName')}")
+# file_handler.setFormatter(format)
 formatter = logging.Formatter(log_format)
 file_handler.setFormatter(formatter)
 log.addHandler(file_handler)
