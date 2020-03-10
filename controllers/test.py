1c764b6eafb63f9c8b39b48e258ba2a92dc7e2c1
Evgeny Grebnev <evg@wallter.com>
2020-03-10 08:35:25 +0200

    html in their directory

--- controllers/test.py
+++ controllers/test.py
@@ -3,6 +3,6 @@ def ok(query):
     if "test" in query:
         if "ok" in query["test"]:
             return "<b>I am the router found answer</b>"
-    fd = open("test.html", "r")
+    fd = open("html/test.html", "r")
     text = fd.read()
     return text
