b017f603e45a3239ae40e68777f3d5ed34e15479
Evgeny Grebnev <evg@wallter.com>
2020-03-11 19:30:37 +0200

    nested added

--- js/script.js
+++ js/script.js
@@ -16,7 +16,10 @@ document.getElementById("add-field").addEventListener("click", function() {
     var clone = template.cloneNode(true)
     clone.removeAttribute("id")
     clone.classList.remove("hide");
-    clone.classList.add("clone-identificator")
+    clone.classList.add("clone-identificator");
+    clone.getElementsByTagName("div")[1].getElementsByClassName("nested")[0].addEventListener("click", function() {
+        open_nested(this);
+    });
     var form = document.getElementById("fields-container").append(clone);
 });
 
@@ -28,4 +31,24 @@ document.getElementById("add-field").addEventListener("click", function() {
 });*/
 
 
+}
+
+function open_nested(elem) {
+    var newrow = document.createElement("div");
+    newrow.classList.add("row");
+    var newcol0 = document.createElement("div");
+    newcol0.classList.add("col-lg-2,col-md-2,col-sm-2");
+    newcol0.innerHTML = "===>";
+    var newinput = document.createElement("input");
+    newinput.classList.add("m-auto");
+    var newcol1 = document.createElement("div");
+    newcol1.classList.add("col-lg-5,col-md-5,col-sm-3");
+    newcol1.appendChild(newinput);
+    var newcol2 = document.createElement("div");
+    newcol2.classList.add("col-lg-5,col-md-5,col-sm-7");
+    newcol2.appendChild(newinput.cloneNode(true));
+    newrow.appendChild(newcol0);
+    newrow.appendChild(newcol1);
+    newrow.appendChild(newcol2);
+    elem.parentElement.parentElement.parentElement.insertBefore(newrow, elem.parentElement.parentElement.nextElementSibling);
 }
