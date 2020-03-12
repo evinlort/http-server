e463270657765c662ddfd6a7fb6d30d99a89930d
Evgeny Grebnev <evg@wallter.com>
2020-03-12 08:14:06 +0200

    adds sub nested row

--- js/script.js
+++ js/script.js
@@ -18,7 +18,11 @@ document.getElementById("add-field").addEventListener("click", function() {
     clone.classList.remove("hide");
     clone.classList.add("clone-identificator");
     clone.getElementsByTagName("div")[1].getElementsByClassName("nested")[0].addEventListener("click", function() {
-        open_nested(this);
+        console.log(clone.getElementsByTagName("div")[0].childNodes[0].value);
+        console.log(clone.getElementsByTagName("div")[1].childNodes[0].value);
+        if(clone.getElementsByTagName("div")[0].childNodes[0].value && !clone.getElementsByTagName("div")[1].childNodes[0].value) {
+            open_nested(this);
+        }
     });
     var form = document.getElementById("fields-container").append(clone);
 });
@@ -36,19 +40,44 @@ document.getElementById("add-field").addEventListener("click", function() {
 function open_nested(elem) {
     var newrow = document.createElement("div");
     newrow.classList.add("row");
+    newrow.classList.add("sub-nested");
+
     var newcol0 = document.createElement("div");
-    newcol0.classList.add("col-lg-2,col-md-2,col-sm-2");
+    newcol0.classList.add("col-lg-2");
+    newcol0.classList.add("col-md-2");
+    newcol0.classList.add("col-sm-2");
     newcol0.innerHTML = "===>";
+
     var newinput = document.createElement("input");
     newinput.classList.add("m-auto");
+
     var newcol1 = document.createElement("div");
-    newcol1.classList.add("col-lg-5,col-md-5,col-sm-3");
+    newcol1.classList.add("col-lg-5");
+    newcol1.classList.add("col-md-5");
+    newcol1.classList.add("col-sm-3");
     newcol1.appendChild(newinput);
+
     var newcol2 = document.createElement("div");
-    newcol2.classList.add("col-lg-5,col-md-5,col-sm-7");
+    newcol2.classList.add("col-lg-5");
+    newcol2.classList.add("col-md-5");
+    newcol2.classList.add("col-sm-7");
     newcol2.appendChild(newinput.cloneNode(true));
+
     newrow.appendChild(newcol0);
     newrow.appendChild(newcol1);
     newrow.appendChild(newcol2);
+
     elem.parentElement.parentElement.parentElement.insertBefore(newrow, elem.parentElement.parentElement.nextElementSibling);
+}
+
+function check_empties(name) {
+    var clones = document.getElementsByClassName(name);
+    while(i++ < clones.length) {
+        console.log(i-1);
+        var divs = clones[i-1].getElementsByTagName("div");
+        if(!divs[0].childNodes[0].value || !divs[1].childNodes[0].value) {
+            return true;
+        }
+    }
+    return false;
 }
