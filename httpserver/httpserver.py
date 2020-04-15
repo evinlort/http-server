49b6b8d2c47088237f471d86e48acd363822f4ed
Evgeny Grebnev <evg@wallter.com>
2020-04-15 13:49:54 +0300

    HS-1 New tests Port is always int

--- httpserver/httpserver.py
+++ httpserver/httpserver.py
@@ -1,10 +1,10 @@
 class HttpServer:
     def __init__(self, *, port: int = 8080):
         print("HttpServer is initialized")
-        self.port = port
+        self.port = int(port)
 
     def set_port(self, port: int):
-        self.port = port
+        self.port = int(port)
 
     def get_port(self):
         return self.port
--- httpserver/tests/test_main.py
+++ httpserver/tests/test_main.py
@@ -1,9 +1,15 @@
+import pytest
+
 import httpserver as h
 
 
 class TestMain:
     def test_instance(self):
         assert "HttpServer" in dir(h)
+
+    def test_default_port(self):
+        hs = h.HttpServer()
+        assert hs.get_port() == 8080
 
     def test_set_port(self):
         hs = h.HttpServer(port=80)
@@ -13,3 +19,9 @@ class TestMain:
         hs = h.HttpServer()
         hs.set_port(80)
         assert hs.get_port() == 80
+
+    def test_faulty_port_definition(self):
+        with pytest.raises(TypeError) as excinfo:
+            # noinspection PyArgumentList
+            h.HttpServer(80)
+        assert "takes 1 positional argument but 2 were given" in str(excinfo.value)
