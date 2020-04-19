1e7832ca878efed5232228ffb782902c615570d9
Evgeny Grebnev <evg@wallter.com>
2020-04-19 09:40:46 +0300

    HS-4 controller for test changed

--- httpserver/tests/controllers/test.py
+++ httpserver/tests/controllers/test.py
@@ -1,8 +1,2 @@
 def ok(query):
     return "<div>Works!</div>"
-    # if "test" in query:
-    #     if "ok" in query["test"]:
-    #         return "<b>I am the router found answer</b>"
-    # fd = open("html/test.html", "r")
-    # text = fd.read()
-    # return text
